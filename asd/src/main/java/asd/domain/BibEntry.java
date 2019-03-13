8f3a7f7dd772c422d11079377064416fe9bff602
Mark Lanser <markklanser@gmail.com>
2019-03-13 10:04:20 +0100

    init

---
+++ asd/src/main/java/asd/domain/BibEntry.java
@@ -0,0 +1,16 @@
+package asd.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * Hello world!
+ *
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+public class BibEntry
+{
+    private long id;
+
+}
---
+++ asd/src/main/java/asd/factories/BibEntryFactory.java
@@ -0,0 +1,20 @@
+package asd.factories;
+
+import asd.domain.BibEntry;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * Hello world!
+ *
+ */
+@NoArgsConstructor
+public class BibEntryFactory
+{
+    // Some factory
+    public BibEntry createBibEntry() 
+    {
+        return null;
+    }
+
+}
